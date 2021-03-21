81b7651a37500f435e547bdc2adc8088adfd7a3d
surya0180 <teja8551@gmail.com>
2021-03-21 15:53:14 +0530

    Changed to Django Framework

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def signin(request):
+    return render(request, 'signin.html')
+def signup(request):
+    return render(request, 'signup.html')
+def docReg(request):
+    return render(request, 'doctor_regestration.html')
+def hospReg(request):
+    return render(request, 'Hospitalregistion.html')
+def userReg(request):
+    return render(request, 'user_registration.html')
+def forgPass(request):
+    return render(request, 'forgot_password.html')
