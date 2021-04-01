f325545f74432162af089daba49cdec46de98896
surya0180 <teja8551@gmail.com>
2021-04-01 22:02:33 +0530

    Created Doctor Table

--- public/settings.py
+++ public/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'JNDB',
         'USER': 'postgres',
-        'PASSWORD': 'Surya_0180',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
