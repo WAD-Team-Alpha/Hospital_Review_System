fa4daf8c275e149cf0ccd3fc1f8c9fcedbf01189
prathyush11 <sprathyush141@gmail.com>
2021-04-01 18:10:33 +0530

    Updated Doctor Registration

--- accounts/views.py
+++ accounts/views.py
@@ -16,3 +16,4 @@ def forgPass(request):
     return render(request, 'forgot_password.html')
 def signinFail(request):
     return render(request, 'signin_fail.html')
+
--- public/settings.py
+++ public/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import pyrebase
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'JNDB',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'sunny',
         'HOST': 'localhost',
     }
 }
