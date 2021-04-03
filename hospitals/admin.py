4cd2483b4dd728fb87a40b3c25f1a214821cbf3f
mahaboob <mahaboob.s19@iiits.in>
2021-04-03 12:42:24 +0530

    added sigin and signout functions and also registerd databases

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Hospital
 # Register your models here.
+
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'HospitalName', 'Username')
+    
+admin.site.register( Hospital ,HospitalAdmin)
