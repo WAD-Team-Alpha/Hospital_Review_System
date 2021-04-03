ef8b83813b24d5dd8d0bd76e1509a4782ae687a9
mahaboob <mahaboob.s19@iiits.in>
2021-04-03 15:31:19 +0530

    added changes in index page after signin page

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from accounts.models import User
 from doctors.models import Doctor
+from hospitals.models import Hospital
+
+
+
 # Create your views here.
 def index(request):
-    users = User.objects.order_by('Username')
+     
+    users = User.objects.all()
+    doctors = Doctor.objects.all()
+    hospitals = Hospital.objects.all()
+    user_list = []
+    doctor_list = []
+    hospital_list = []
+    for user in users:
+       
+        user_list.append(user.Username)
+    for doctor in doctors:
+        
+        doctor_list.append(doctor.Username)
+    for hospital in hospitals:
+        
+        hospital_list.append(hospital.Username)
     context = {
-        'users' : users
+        'users' : user_list,
+        'doctors' : doctor_list,
+        'hospitals' : hospital_list
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
