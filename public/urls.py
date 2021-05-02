3a6a7073526a9b1e8a180bcceb11070815f364d5
surya0180 <teja8551@gmail.com>
2021-05-02 10:17:02 +0530

    Completed forgot password section

--- public/urls.py
+++ public/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('mainpage.urls')),
@@ -25,6 +26,10 @@ urlpatterns = [
     path('hospitals/', include('hospitals.urls')),
     path('reviews/', include('reviews.urls')),
     path('admin/', admin.site.urls),
-]
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='Email.html'), name='password_reset'),
+    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='forgot_password.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+]  
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
